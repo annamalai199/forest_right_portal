b25d1ee5e52a9fa8badf30bdfbf85930f69d10d7
annamalai199 <sriannamalai1909@gmail.com>
2025-09-16 19:19:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/forest_right_portal.github.io/" : "/", // 👈 important for GitHub Pages
+  base: "/forest_right_portal.github.io/",  // 👈 important
   server: {
     host: "::",
     port: 8080,
